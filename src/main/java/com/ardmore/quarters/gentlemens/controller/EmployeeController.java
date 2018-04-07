2782181dd23fe872558d1695dde7ae691ad0e50d
Dan R <daniel.ries@ymail.com>
2018-04-07 19:05:44 -0400

    Uncommented employee controller

--- src/main/java/com/ardmore/quarters/gentlemens/controller/EmployeeController.java
+++ src/main/java/com/ardmore/quarters/gentlemens/controller/EmployeeController.java
@@ -1,65 +1,54 @@
-//package com.ardmore.quarters.gentlemens.controller;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.util.UriComponentsBuilder;
-//
-//import com.ardmore.quarters.gentlemens.entity.Employee;
-//import com.ardmore.quarters.gentlemens.service.EmployeeServiceImpl;
-//
-//@RestController
-//@RequestMapping("/createEmployee")
-//public class EmployeeController {
-//
-//@Autowired
-//private EmployeeServiceImpl employeeService;
-////read
-//@GetMapping("/employees")
-//public ResponseEntity<List<Employee>> getAllEmployees(){
-//	List<Employee> employeeList = employeeService.getAllEmployees();
-//	return new ResponseEntity<List<Employee>>(employeeList,HttpStatus.OK);
-//}
-////read
-//@GetMapping("/emloyee/{id}")
-//public ResponseEntity<Employee> getEmployeeByID(@PathVariable("id") Integer id){
-//	Employee employee = employeeService.getEmployeeById(id);
-//	return new ResponseEntity<Employee>(employee,HttpStatus.OK);
-//}
-////create
-//@PostMapping("/employee")
-//public ResponseEntity<Void> addEmployee(@RequestBody Employee employee, UriComponentsBuilder builder){
-//	boolean newEmployee = employeeService.addEmployee(employee);
-//	if(newEmployee == false){
-//		return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-//	}
-//	HttpHeaders headers = new HttpHeaders();
-//	headers.setLocation(builder.path("/employee/{id}").buildAndExpand(employee.getEmployeeId()).toUri());
-//	return new ResponseEntity<Void>(HttpStatus.CREATED);
-//}
-//
-////update
-//@PutMapping("/employee")
-//public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
-//	employeeService.updateEmployee(employee);
-//	return new ResponseEntity<Employee>(employee, HttpStatus.OK);
-//}
-//
-//@DeleteMapping("employee/{id}")
-//public ResponseEntity<Void> deleteEmployee(@PathVariable("id") Integer id){
-//	employeeService.deleteEmployee(id);
-//	return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-//}
-//
-//}
+package com.ardmore.quarters.gentlemens.controller;
+
+import com.ardmore.quarters.gentlemens.entity.Employee;
+import com.ardmore.quarters.gentlemens.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/createEmployee")
+public class EmployeeController {
+
+@Autowired
+private EmployeeServiceImpl employeeService;
+
+
+@GetMapping("/employees")
+public ResponseEntity<Iterable<Employee>> getAllEmployees(){
+	Iterable<Employee> employeeList = employeeService.getAllEmployees();
+	return new ResponseEntity<>(employeeList,HttpStatus.OK);
+}
+
+
+@GetMapping("/emloyee/{id}")
+public ResponseEntity<Employee> getEmployeeByID(@PathVariable("id") Integer id){
+	Employee employee = employeeService.getEmployeeById(id);
+	return new ResponseEntity<>(employee,HttpStatus.OK);
+}
+
+
+@PostMapping(path = "/employee")
+public ResponseEntity<Void> addEmployee(@RequestBody Employee employee){
+	boolean newEmployee = employeeService.addEmployee(employee);
+	if(!newEmployee){
+		return new ResponseEntity<>(HttpStatus.CONFLICT);
+	}
+	return new ResponseEntity<>(HttpStatus.CREATED);
+}
+
+
+@PutMapping("/employee")
+public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+	employeeService.updateEmployee(employee);
+	return new ResponseEntity<>(employee, HttpStatus.OK);
+}
+
+@DeleteMapping("employee/{id}")
+public ResponseEntity<Void> deleteEmployee(@PathVariable("id") Integer id){
+	employeeService.deleteEmployee(id);
+	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+}
+
+}
